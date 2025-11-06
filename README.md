6c19318c2d84ea57033eab52ead4019ac6cea047
watson-xd-2 <watsonxdboy@gmail.com>
2025-11-06 05:24:17 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,86 @@
+<p align="center">
+    <img src="./thumbnail.jpg" width="100%" style="margin-left: auto;margin-right: auto;display: block;">
+</p>
+<h1 align="center">InoriMD</h1>
+
+</p>
+<p align="center">
+<a href="https://github.com/svazerID"><img title="Author" src="https://img.shields.io/badge/AUTHOR-svazerID-green.svg?style=for-the-badge&logo=github"></a>
+
+##
+[![BOT WHATSAPP](https://img.shields.io/badge/WhatsApp%20BOT-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://api.whatsapp.com/send/?phone=6283129667247&text=.menu)
+[![CHANNEL](https://img.shields.io/badge/Channel-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://whatsapp.com/channel/0029VbAhesDJ3jutFwGpwE3o)
+[![ONWER](https://img.shields.io/badge/Owner%20BOT-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://wa.me/6283129667247)
+ 
+
+
+
+## NOTE BEFORE USING!!
+- Edit all Owner Information on config.example.js
+- Dont forget to rename `config.example.js` to `config.js`
+- If the pairing code not appear in your terminal, simply go to `/sessions/creds.json` and you will see the code
+
+
+### a little about this bot
+- ✔️ | **Simple** 
+- ✔️ | **Button** 
+- ✔️ | **Multi Device** 
+- ✔️ | **Plugins** 
+- ✔️ | **Style Code** 
+- ✔️ | **No Prefix** 
+---------
+### Some of the features include
+- ✔️ | Welcome & Leave
+- ✔️ | AI Feature
+- ✔️ | Sticker 
+- ✔️ | Downloader 
+- ✔️ | Anime
+- ✔️ | Tools 
+---------
+
+## Requirements
+
+* NodeJS V20+
+* FFMPEG
+* Imagemagick
+* NodeJS basic knowledges (dont be an idiot)
+
+#### Setup bot on your Machine
+```bash
+$ git clone https://github.com/svazerID/InoriMD
+$ cd InoriMD
+$ npm i
+```
+
+---------
+
+## Run
+
+```bash
+npm start
+```
+
+---------
+
+#
+### Terms
+1. Dont sell this script!
+2. Dont forget to star this repository!
+3. Follow my GitHub!
+4. Use with your own risk!
+
+---------
+
+### Thanks To
+[![Nurutomo](https://github.com/Nurutomo.png?size=100)](https://github.com/Nurutomo)
+[![WhiskeySockets](https://github.com/WhiskeySockets.png?size=100)](https://github.com/WhiskeySockets/Baileys)
+[![BochilGaming](https://github.com/BochilGaming.png?size=100)](https://github.com/BochilGaming)
+[![ImYanXiao](https://github.com/ImYanXiao.png?size=100)](https://github.com/ImYanXiao)
+[![Xnuvers007](https://github.com/Xnuvers007.png?size=100)](https://github.com/Xnuvers007)
+[![ShirokamiRyzen](https://github.com/ShirokamiRyzen.png?size=100)](https://github.com/ShirokamiRyzen)
+[![AgusXzz](https://github.com/AgusXzz.png?size=100)](https://github.com/AgusXzz)
+[![MythEclipse](https://github.com/MythEclipse.png?size=100)](https://github.com/MythEclipse)
+#### Contributor
+[![svazerID](https://github.com/svazerID.png?size=100)](https://github.com/svazerID)
+
+---------
---
+++ config.js
@@ -0,0 +1,190 @@
+/*
+Author : Shirokami Ryzen 
+WA : +6285174269046
+Base : Elaina-MultiDevice
+Release : 22 Nov 2022
+*/
+
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import moment from 'moment-timezone'
+
+/*============= WAKTU =============*/
+let wktuwib = moment.tz('Africa/Harare').format('HH:mm:ss') + ' WIB';
+let wktuwita = moment.tz('Africa/Harare').format('HH:mm:ss') + ' WITA';
+let wktuwit = moment.tz('Africa/Harare').format('HH:mm:ss') + ' WIT';
+global.gabung = wktuwib + '\n' + wktuwita + '\n' + wktuwit;
+let d = new Date(new Date + 3600000)
+let locale = 'id'
+
+let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, {
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric'
+});
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+/*============= MAIN INFO =============*/
+global.pairing = '263781330745'
+global.owner = [
+    ['263781330745', 'watson-xd', 'true']
+]
+global.mods = ['263781330745']
+global.prems = []
+global.nomorbot = '263781330745'
+global.nomorown = '263781330745'
+
+/*============= WATERMARK =============*/
+global.readMore = readMore
+global.author = 'watsonxd'
+global.namebot = 'WATSON-XD-BOT'
+global.wm = '© WATSON-XD-BOT'
+global.watermark = wm
+global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
+global.bottime = `T I M E : ${wktuwib}`
+global.stickpack = `Sticker by watson fourpence:`
+global.stickauth = `${namebot}\n at: ${week} ${date}\n⫹⫺ time: ${wktuwib}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n© ${author}`
+global.week = `${week} ${date}`
+global.wibb = `${wktuwib}`
+
+/*============== SOCIAL ==============*/
+global.sig = 'https://www.instagram.com/watson-xd3'
+global.sgh = 'https://github.com/watson-xd7'
+global.sgc = 'https://whatsapp.com/channel/0029Vb5m3D9A2pL6kvlwSf2S'
+global.sgw = 'https://github.com/watson-xd7'
+global.sdc = '-'
+global.sfb = 'https://www.facebook.com/profile.php?id=100093026894360&mibextid=ZbWKwL'
+global.snh = 'https://www.instagram.com/watson-xd3'
+
+/*============= BOT FEATURES =============*/
+global.autotyping = true       // default off
+global.autorecording = false    // default off
+/*============= RESPON =============*/
+global.wait = 'Please Wait...'
+global.eror = 'Error!'
+
+/*============= API =============*/
+global.APIs = {
+  ryzumi: 'https://api-02.ryzumi.vip',
+
+}
+
+/*============= API KEY =============*/
+global.APIKeys = {
+  // 'https://website': 'apikey'
+}
+
+/*============== LOGO ==============*/
+global.thumb = 'https://telegra.ph/file/cce9ab4551f7150f1970d.jpg' //Main Thumbnail
+global.thumb2 = 'https://telegra.ph/file/26b515d170f1e599f78a7.jpg'
+global.thumbbc = 'https://telegra.ph/file/05f874dc87f7e27fa8127.jpg' //For broadcast
+global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
+global.thumblvlup = 'https://telegra.ph/file/a3e66e0fa840b08236c75.jpg'
+
+/*=========== TYPE DOCUMENT ===========*/
+global.dpptx = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.ddocx = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.dxlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.dpdf = 'application/pdf'
+global.drtf = 'text/rtf'
+global.djson = 'application/json'
+
+/*=========== HIASAN ===========*/
+// DEFAULT MENU
+global.dmenut = 'ଓ═┅═━–〈' //top
+global.dmenub = '┊↬' //body
+global.dmenub2 = '┊' //body for info cmd on Default menu
+global.dmenuf = '┗––––––––––✦' //footer
+
+// COMMAND MENU
+global.dashmenu = '┅━━━═┅═❏ *ღ *DASHBOARD* ღ* ❏═┅═━━━┅'
+global.cmenut = '❏––––––『'                       //top
+global.cmenuh = '』––––––'                        //header
+global.cmenub = '┊❀'                            //body
+global.cmenuf = '┗━═┅═━––––––๑\n'                //footer
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     ' //after
+global.pmenus = '┊'                              //pembatas menu selector
+
+global.htki = '––––––『' // Hiasan Titile (KIRI)
+global.htka = '』––––––' // Hiasan Title  (KANAN)
+global.lopr = 'Ⓟ' //LOGO PREMIUM ON MENU.JS
+global.lolm = 'Ⓛ' //LOGO LIMIT/FREE ON MENU.JS
+global.htjava = '⫹⫺'    //hiasan Doang :v
+global.hsquere = ['⛶', '❏', '⫹⫺']
+
+global.multiplier = 100
+
+global.rpg = {
+    emoticon(string) {
+        string = string.toLowerCase()
+        let emot = {
+            level: '📊',
+            limit: '🎫',
+            health: '❤️',
+            exp: '✨',
+            atm: '💳',
+            money: '💰',
+            bank: '🏦',
+            potion: '🥤',
+            diamond: '💎',
+            common: '📦',
+            uncommon: '🛍️',
+            mythic: '🎁',
+            legendary: '🗃️',
+            superior: '💼',
+            pet: '🔖',
+            trash: '🗑',
+            armor: '🥼',
+            sword: '⚔️',
+            pickaxe: '⛏️',
+            fishingrod: '🎣',
+            wood: '🪵',
+            rock: '🪨',
+            string: '🕸️',
+            horse: '🐴',
+            cat: '🐱',
+            dog: '🐶',
+            fox: '🦊',
+            robo: '🤖',
+            petfood: '🍖',
+            iron: '⛓️',
+            gold: '🪙',
+            emerald: '❇️',
+            upgrader: '🧰',
+            bibitanggur: '🌱',
+            bibitjeruk: '🌿',
+            bibitapel: '☘️',
+            bibitmangga: '🍀',
+            bibitpisang: '🌴',
+            anggur: '🍇',
+            jeruk: '🍊',
+            apel: '🍎',
+            mangga: '🥭',
+            pisang: '🍌',
+            botol: '🍾',
+            kardus: '📦',
+            kaleng: '🏮',
+            plastik: '📜',
+            gelas: '🧋',
+            chip: '♋',
+            umpan: '🪱',
+            skata: '🧩'
+        }
+        let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+        if (!results.length) return ''
+        else return emot[results[0][0]]
+    }
+}
+
+//------ JANGAN DIUBAH -----
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
